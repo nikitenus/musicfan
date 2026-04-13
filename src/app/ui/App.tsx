113c7eaf127c83e7f97144c0e1f3d08355a30119
vlad <nikitenuss@mail.ru>
2026-04-13 13:11:20 +0300

    feat: add Header

---
+++ src/app/ui/App.tsx
@@ -0,0 +1,14 @@
+import "../../App.css"
+import { Routing } from "@/common/routing/Routing.tsx"
+import { Header } from "@/common/components/Header/Header.tsx"
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Routing />
+    </>
+  )
+}
+
+export default App
